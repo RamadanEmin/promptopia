2d6314a18c4c638424bab63d66694340ee6cca79
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-25 22:54:22 +0300

    Added authentication api

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    callbacks: {
+        async session({ session }) {
+            
+        },
+        async signIn({ profile }) {
+            try {
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        }
+    }
+})
+
+export { handler as GET, handler as POST };
