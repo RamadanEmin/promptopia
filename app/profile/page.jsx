3b24859a43d9fdbe290d1f9b41bdaeaa47162138
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-31 11:28:30 +0300

    Added 'Profile page'

---
+++ app/profile/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+      data={myPosts}
+    />
+  );
+};
+
+export default MyProfile;
