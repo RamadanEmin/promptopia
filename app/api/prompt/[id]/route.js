f42f5fe360ee258da9111e6982bc2d211d8cedd3
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-06-02 15:58:40 +0300

    Added Delete endpoint

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -37,3 +37,15 @@ export const PATCH = async (request, { params }) => {
         return new Response("Error Updating Prompt", { status: 500 });
     }
 };
+
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 });
+    } catch (error) {
+        return new Response("Error deleting prompt", { status: 500 });
+    }
+};
